db15aea42905d77b9c7f5540daed60dc461f2de7
AndresGarciaT <antoga721@gmail.com>
2023-05-30 22:45:49 -0500

    [Create]avance

--- carrito-react-main/src/data.js
+++ carrito-react-main/src/data.js
@@ -8,7 +8,7 @@ export const data = [
 	},
 	{
 		id: 2,
-		img: 'https://us.123rf.com/450wm/oloya/oloya1704/oloya170400052/76465691-pera-roja-fresca-sobre-fondo-azul-comida-de-frutas-y-vegetales-de-estilo-moderno-dise%C3%B1o-de.jpg?ver=6',
+		img: 'https://us.123rf.com/450wm/badboydt7/badboydt71510/badboydt7151000077/46528068-fondo-de-color-s%C3%B3lido-disparo-de-pera.jpg?ver=6',
 		nameProduct: 'Pera',
 		price: 1100,
 		quantity: 1,
@@ -32,6 +32,38 @@ export const data = [
 		img: 'https://thumbs.dreamstime.com/b/mango-entero-levitando-contra-fondo-del-mismo-color-un-maduro-en-el-aire-amarillo-composici%C3%B3n-simple-monocrom%C3%A1tica-de-fruta-216011696.jpg',
 		nameProduct: 'Mango',
 		price: 2000,
+		quantity: 1,
+	},
+
+	{
+		id: 6,
+		img: 'https://thumbs.dreamstime.com/b/jugo-delicioso-de-lulo-en-fondo-rojo-61533426.jpg',
+		nameProduct: 'Jugo de Lulo',
+		price: 2500,
+		quantity: 1,
+	},
+	
+	{
+		id: 7,
+		img: 'https://img.freepik.com/fotos-premium/jugo-guayaba-roja-servido-vaso-encima-mesa-madera-negra-copie-espacio-texto-o-publicidad_511235-5071.jpg?w=2000',
+		nameProduct: 'Jugo de Guayaba',
+		price: 2700,
+		quantity: 1,
+	},
+
+	{
+		id: 8,
+		img: 'https://www.recetasparavivirmejor.com/wp-content/uploads/2021/02/recetas_para_vivir_mejor_jugo_de_mora_con_leche_batidos_2021-1.jpg',
+		nameProduct: 'Jugo de Mora',
+		price: 2900,
+		quantity: 1,
+	},
+
+	{
+		id: 9,
+		img: 'https://nomadasgourmet.com/wp-content/uploads/2023/03/receta-beneficios-jugo-arandanos-7409312.jpg',
+		nameProduct: 'Jugo de Arandanos',
+		price: 3200,
 		quantity: 1,
 	},
 	
